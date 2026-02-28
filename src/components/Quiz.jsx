c4101f49b6b60166c73bdeaec28b15063cfa8d3e
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2026-02-28 11:59:09 +0200

    Project
    Deriving Values, Outputting Questions & Registering Answers

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import QUESTION from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
+
+const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTION.length;
+
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  };
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTION[activeQuestionIndex].answers].sort(
+    () => Math.random() - 0.5,
+  );
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTION[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
